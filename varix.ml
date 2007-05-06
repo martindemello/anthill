756ff918136046adcb79614cfef5f6f66ae258c2
martin@localhost <martin@localhost>
2007-05-06 23:46:19 +0530

    renamed project to varix

--- varix.ml
+++ varix.ml
@@ -253,11 +253,16 @@ let sort_by f l =
 
 let anagrams_of_string str = anagrams (Bag.of_string str) start;;
 
-let pattern_of_string str = pattern (explode str) start;;
+let patterns_of_string str = pattern (explode str) start;;
 
 (*************************************************************************
  * main() and friends
  * ***********************************************************************)
+
+type eval = { desc: string; proc: string -> string list };;
+
+let anag = { desc = "anagram > "; proc = anagrams_of_string };;
+let patt = { desc = "pattern > "; proc = patterns_of_string };;
 
 let readline prompt =
   Ledit.set_prompt prompt;
@@ -269,20 +274,25 @@ let readline prompt =
   loop (Ledit.input_char stdin);;
 
 let _ = 
+  print_endline "Anagram: {letters} or a letters";
+  print_endline "Pattern: [letters] or p letters";
+  print_endline "Use . for a blank and * for any number of blanks";
+  print_endline "";
   print_endline "Hit Ctrl-D to exit";
-  print_endline "------------------";
+  print_endline "------------------------------------------------";
   flush stdout;
-  try while true do
-    let str = readline "sowpods > " in
-
-    let output = match str with
-    | RE "{" (_* as anag) "}" -> anagrams_of_string anag
-    | RE ['a' 'A'] space (_* as anag) -> anagrams_of_string anag
-    | RE "[" (_* as patt) "]" -> pattern_of_string patt
-    | RE ['p' 'P'] space (_* as patt) -> pattern_of_string patt
-    | _ -> [];
-    in
-    List.iter (printf "%s\n") (sort_by caps_in output);
-    flush stdout;
-  done
-  with End_of_file -> print_newline ();;
+  let cur = ref anag in
+  try 
+    while true do
+      let str = readline !cur.desc in
+      let input = match str with
+      | RE "{" (_* as inp) "}" -> cur := anag; inp
+      | RE "[" (_* as inp) "]" -> cur := patt; inp
+      | RE ['a' 'A'] space (_* as inp) -> cur := anag; inp
+      | RE ['p' 'P'] space (_* as inp) -> cur := patt; inp
+      | _ -> str
+      in
+      List.iter (printf "%s\n") (sort_by caps_in (!cur.proc input));
+      flush stdout;
+    done
+      with End_of_file -> print_newline ();;
