d3a569f01e1f404e19f645cc788852f5305491d0
Martin DeMello <martindemello@gmail.com>
2009-10-31 14:17:02 +0530

    factor out the accumulator and the dawg traversal functions

--- search.ml
+++ search.ml
@@ -15,43 +15,47 @@ let check str =
   in
   walk start_node 0;;
 
-(* follow a 'trail' of characters or wildcards starting from a given prefix *)
-let pattern trail path =
+let collecting traversal =
   let retval = ref [] in
   let add_word = function [] -> () | s::_ -> retval := s :: !retval in
-  let rec traverse trail path =
-    match trail with
-    |[]        -> ();
-    |'.' :: cs -> foreach_sib (follow cs) path
-    |'*' :: cs -> ( traverse cs path; foreach_sib (follow trail) path; )
-    |c   :: cs -> try follow cs (sib c path) with Not_found -> ()
-  and follow tr pth =
-    let try_step tr pth = try traverse tr (step pth) with Not_found -> () in
-    match tr with
-    |[]    -> add_word (word_of pth);
-    |['*'] -> ( add_word (word_of pth); try_step tr pth; )
-    |_     -> try_step tr pth
-  in
-    traverse trail path;
-    List.rev !retval
+  traversal add_word;
+  List.rev !retval
+
+(* follow a 'trail' of characters or wildcards starting from a given prefix *)
+let pattern trail path =
+  let traversal add_word =
+    let rec traverse trail path =
+      match trail with
+      |[]        -> ();
+      |'.' :: cs -> foreach_sib (follow cs) path
+      |'*' :: cs -> ( traverse cs path; foreach_sib (follow trail) path; )
+      |c   :: cs -> try follow cs (sib c path) with Not_found -> ()
+    and follow tr pth =
+      let try_step tr pth = try traverse tr (step pth) with Not_found -> () in
+      match tr with
+      |[]    -> add_word (word_of pth);
+      |['*'] -> ( add_word (word_of pth); try_step tr pth; )
+      |_     -> try_step tr pth
+    in
+      traverse trail path;
+  in collecting traversal
 ;;
 
 (* Build all possible words from a bag and a dawg *
  * if all = false, return only words using the entire bag *)
 let build bag path all =
-  let retval = ref [] in
-  let add_word = function [] -> () | s::_ -> retval := s :: !retval in
-  let rec traverse bag path =
-    foreach_sib (follow bag) path
-  and follow bag path =
-    let new_bag, played = Bag.play (letter path.node) bag in
-    if Bag.is_empty new_bag then add_word (uword_of path played)
-    else
-      (if all then add_word (uword_of path played) else ());
-      try traverse new_bag (ustep path played) with Not_found -> ()
-  in
-    traverse bag path;
-    List.rev !retval
+  let traversal add_word =
+    let rec traverse bag path =
+      foreach_sib (follow bag) path
+    and follow bag path =
+      let new_bag, played = Bag.play (letter path.node) bag in
+      if Bag.is_empty new_bag then add_word (uword_of path played)
+      else
+        (if all then add_word (uword_of path played) else ());
+        try traverse new_bag (ustep path played) with Not_found -> ()
+    in
+      traverse bag path;
+  in collecting traversal
 ;;
 
 let anagrams bag path = build bag path false;;
