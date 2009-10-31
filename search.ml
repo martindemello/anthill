4aa91e61abe6a2532828164073f9f38c9cbe2ce4
Martin DeMello <martindemello@gmail.com>
2009-10-31 13:46:45 +0530

    rewrite pattern and build to expose their common structure

--- search.ml
+++ search.ml
@@ -19,46 +19,39 @@ let check str =
 let pattern trail path =
   let retval = ref [] in
   let add_word = function [] -> () | s::_ -> retval := s :: !retval in
-  let rec pattern' trail path =
-    let try_step tr pth = try pattern' tr (step pth) with Not_found -> () in
-    let follow tr pth =
-      match tr with
-      |[]    -> add_word (word_of pth);
-      |['*'] -> ( add_word (word_of pth); try_step tr pth; )
-      |_     -> try_step tr pth
-    in
+  let rec traverse trail path =
     match trail with
     |[]        -> ();
     |'.' :: cs -> foreach_sib (follow cs) path
-    |'*' :: cs -> ( pattern' cs path; foreach_sib (follow trail) path; )
+    |'*' :: cs -> ( traverse cs path; foreach_sib (follow trail) path; )
     |c   :: cs -> try follow cs (sib c path) with Not_found -> ()
-  in 
-    pattern' trail path;
+  and follow tr pth =
+    let try_step tr pth = try traverse tr (step pth) with Not_found -> () in
+    match tr with
+    |[]    -> add_word (word_of pth);
+    |['*'] -> ( add_word (word_of pth); try_step tr pth; )
+    |_     -> try_step tr pth
+  in
+    traverse trail path;
     List.rev !retval
 ;;
 
-
 (* Build all possible words from a bag and a dawg *
- * if all = false, return only words using the entire bag
- *
- * TODO: same 'too-generous exception' fix that pattern needed
- * *)
+ * if all = false, return only words using the entire bag *)
 let build bag path all =
   let retval = ref [] in
   let add_word = function [] -> () | s::_ -> retval := s :: !retval in
   let rec traverse bag path =
-    foreach_sib (follow_if bag) path
-  and follow_if bag path =
-    try 
-      let new_bag, played = Bag.play (letter path.node) bag in
-      if Bag.is_empty new_bag then add_word (uword_of path played)
-      else 
-        (if all then add_word (uword_of path played) else ());
-        traverse new_bag (ustep path played)
-    with Not_found -> ()
- in
- traverse bag path;
- List.rev !retval
+    foreach_sib (follow bag) path
+  and follow bag path =
+    let new_bag, played = Bag.play (letter path.node) bag in
+    if Bag.is_empty new_bag then add_word (uword_of path played)
+    else
+      (if all then add_word (uword_of path played) else ());
+      try traverse new_bag (ustep path played) with Not_found -> ()
+  in
+    traverse bag path;
+    List.rev !retval
 ;;
 
 let anagrams bag path = build bag path false;;
