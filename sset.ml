30228385d08efabcc9c821a3435439e2300f16e0
Martin DeMello <martindemello@gmail.com>
2010-05-21 18:19:40 +0530

    don't lowercase when adding to set; it loses blank info

--- sset.ml
+++ sset.ml
@@ -3,8 +3,7 @@ module StringSet = Set.Make(String);;
 let empty = StringSet.empty;;
 
 let of_list lst =
-  List.fold_left (fun set i -> 
-    StringSet.add (String.lowercase i) set) empty lst;;
+  List.fold_left (fun set i -> StringSet.add i set) empty lst;;
 
 let to_list set =
   StringSet.elements set;;
