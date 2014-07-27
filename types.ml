a283badd23cce351ca25154255a859657ce0a054
Martin DeMello <martindemello@gmail.com>
2014-07-26 18:01:24 -0700

    rename types.node to tile to not conflict with trie.node

--- types.ml
+++ types.ml
@@ -1,14 +1,14 @@
 type group = Group.CharSet.t;;
 
-type node = Letter of char | Group of group | Dot | Star
+type tile = Letter of char | Group of group | Dot | Star
 
-let char_of_node n = match n with
+let char_of_tile n = match n with
 | Letter c -> c
 | Dot -> '.'
 | Star -> '*'
 | Group _ -> '?'
 
-let node_of_char c = match c with
+let tile_of_char c = match c with
 | '.' -> Dot
 | '*' -> Star
 | c   -> Letter c
